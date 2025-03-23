e5558050d5ecf387e1f42a552f45b3dd345f96b3
sifis <ibolov.dev@gmail.com>
2025-03-23 11:13:01 +0200

    Add project files.

---
+++ MyFirstAPI/Program.cs
@@ -0,0 +1,50 @@
+
+using Microsoft.EntityFrameworkCore;
+using MyFirstAPI.Models;
+
+namespace MyFirstAPI
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+
+			builder.Services.AddControllers();
+			//builder.Services.AddControllers()
+			//								.ConfigureApiBehaviorOptions(options =>
+			//								{
+			//									options.SuppressModelStateInvalidFilter = true;
+			//								});
+
+			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
+
+			builder.Services.AddDbContext<ShopContext>(options =>
+			{
+				options.UseInMemoryDatabase("Shop");
+			});
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseAuthorization();
+
+
+			app.MapControllers();
+
+			app.Run();
+		}
+	}
+}
